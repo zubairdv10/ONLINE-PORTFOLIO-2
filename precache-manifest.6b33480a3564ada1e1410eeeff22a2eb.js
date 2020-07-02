4490d31356a8c6cb5bae02aa9201f20067a3a99a
zubairdv10 <zubairdevries127@gmail.com>
2020-07-02 14:31:54 +0200

    Updates

---
+++ precache-manifest.6b33480a3564ada1e1410eeeff22a2eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a7e963895db470d2adeac8ce60a41c",
+    "url": "/Online-Porfolio/index.html"
+  },
+  {
+    "revision": "3a1166349feb8c4371b3",
+    "url": "/Online-Porfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "87efd1eb4ecdd06045d9",
+    "url": "/Online-Porfolio/static/css/main.c426a38f.chunk.css"
+  },
+  {
+    "revision": "3a1166349feb8c4371b3",
+    "url": "/Online-Porfolio/static/js/2.43e05cb0.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/Online-Porfolio/static/js/2.43e05cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87efd1eb4ecdd06045d9",
+    "url": "/Online-Porfolio/static/js/main.340e83d5.chunk.js"
+  },
+  {
+    "revision": "09d196d16454e2bdf3f4",
+    "url": "/Online-Porfolio/static/js/runtime-main.69fd7bd8.js"
+  },
+  {
+    "revision": "83f00375c69e7b8b428346f20274505f",
+    "url": "/Online-Porfolio/static/media/pic.83f00375.png"
+  }
+]);
