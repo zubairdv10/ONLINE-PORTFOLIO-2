444620568891ff7dc77ba15939cd011ace34b9b6
zubairdv10 <zubairdevries127@gmail.com>
2020-07-24 17:52:07 +0200

    Update

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import LandingPage from "./landingpage";
+import Landing from "./landingpage";
 import AboutMe from "./aboutMe";
 import Contact from "./contact";
 import Resume from "./resume";
@@ -9,8 +9,8 @@ import Testamonials2 from "./Testamonials2";
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={LandingPage} />
-    <Route path="/aboutme" component={AboutMe} />
+    <Route exact path="/" component={Landing} />
+    <Route exact path="/aboutme" component={AboutMe} />
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
     <Route path="/resume" component={Resume} />
