0f7f7347b057f2728a55837a5a0ba496f0d10525
zubairdv10 <zubairdevries127@gmail.com>
2020-07-26 13:10:21 +0200

    Update

--- src/components/main.js
+++ src/components/main.js
@@ -9,8 +9,8 @@ import Testamonials2 from "./Testamonials2";
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={Landing} />
-    <Route exact path="/aboutme" component={AboutMe} />
+    <Route path="/" component={Landing} />
+    <Route path="/aboutme" component={AboutMe} />
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
     <Route path="/resume" component={Resume} />
