6b483bef77548ce9a3e9e85a70572eac798ca49d
zubairDv10 <zubairdevries127@gmail.com>
2021-01-29 13:54:47 +0200

    changed

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -14,7 +14,7 @@ class Landing extends Component {
 
               <hr />
 
-              <p>HTML/CSS || React || Python || JavaScript</p>
+              <p>HTML/CSS || React || Python || JavaScript || WordPress</p>
               <div className="social-links">
                 <a
                   href="https://github.com/zubairdv10"
